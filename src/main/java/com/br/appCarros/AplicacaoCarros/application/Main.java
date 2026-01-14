fcbf2beec5aa12a30c15a3f283c2577c7f500cc4
Andreas Santos <andreassantos.3003@gmail.com>
2026-01-14 14:35:17 -0300

    Função de cadastrar um veículo

--- src/main/java/com/br/appCarros/AplicacaoCarros/application/Main.java
+++ src/main/java/com/br/appCarros/AplicacaoCarros/application/Main.java
@@ -15,10 +15,12 @@ import com.br.appCarros.AplicacaoCarros.service.FipeApi;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -46,6 +48,7 @@ public class Main {
             System.out.println("Escolha a opção desejada:");
             System.out.println("1 - Cadastrar Cliente");
             System.out.println("2 - Listar Clientes");
+            System.out.println("3 - Cadastrar Veículo");
             System.out.println("7 - Cadastrar Vendedor");
             System.out.println("8 - Listar Vendedores");
             System.out.println("9 - Buscar vendedor");
@@ -60,6 +63,9 @@ public class Main {
                 case 2:
                     getCostumers();
                     break;
+                case 3:
+                    registerVehicle();
+                    break;
                 case 7:
                     registerSalesman();
                     break;
@@ -73,6 +79,132 @@ public class Main {
                     System.out.println("Sistema encerrado!");
                     break;
             }
+        }
+    }
+
+    private void registerVehicle() {
+        var jsonBrands = fipeApi.getBrands("cars");
+        var brands = dataConverter.getList(jsonBrands, BrandData.class);
+
+        System.out.println("Digite a marca do veículo:");
+        String brandSearch = scan.nextLine();
+
+        Optional<List<BrandData>> optionalBrandData = Optional.of(brands.stream()
+                .filter(b -> b.brandName().toUpperCase().contains(brandSearch.toUpperCase()))
+                .collect(Collectors.toList()));
+
+        if(optionalBrandData.isEmpty()) {
+            System.out.println("Não foi encontrada marcas com o nome digitado!");
+            return;
+        }
+
+        optionalBrandData.get().forEach(
+                b -> System.out.println("Código: " + b.brandCode() + " Marca: " + b.brandName()));
+        System.out.println("Digite o código da marca desejado:");
+        int brandId = scan.nextInt();
+        scan.nextLine();
+
+        Optional<BrandData> brandFiltered = optionalBrandData.get().stream()
+                .filter(b -> b.brandCode().equals(brandId))
+                .findFirst();
+
+        if(brandFiltered.isEmpty()) {
+            System.out.println("Não foi encontrada uma marca com o código digitado!");
+            return;
+        }
+
+        System.out.println("Qual o ano do veículo?");
+        int yearSearch = scan.nextInt();
+        scan.nextLine();
+
+        System.out.println(
+                """
+                    Digite o número do combustível:
+                    1 - Gasolina
+                    2 - Álcool
+                    3 - Diesel
+                    4 - Elétrico
+                    5 - Flex
+                    6 - Híbrido   
+                """
+        );
+        int fuelId = scan.nextInt();
+        scan.nextLine();
+
+        var jsonYears = fipeApi.getYearsByProducer("cars", brandId);
+        var years = dataConverter.getList(jsonYears, YearData.class);
+
+        String yearId = yearSearch + "-" + fuelId;
+
+        Optional<YearData> yearFiltered = years.stream().
+                filter(y -> y.yearCode().equals(yearId))
+                .findFirst();
+
+        if(yearFiltered.isEmpty()) {
+            System.out.println("Não foi encontrado veículos desse ano/combustível para a marca "
+                    + brandFiltered.get().brandName());
+            return;
+        }
+
+        System.out.println("Digite o modelo do veículo:");
+        String modelSearch = scan.nextLine();
+
+        var jsonModels = fipeApi.getModelsByProducerAndYear("cars", brandId, yearId);
+        var models = dataConverter.getList(jsonModels, ModelData.class);
+
+        Optional<List<ModelData>> optionalModelData = Optional.of(models.stream()
+                .filter(m -> m.modelName().toUpperCase().contains(modelSearch.toUpperCase()))
+                .collect(Collectors.toList()));
+
+        if(optionalModelData.isEmpty()) {
+            System.out.println("Modelo digitado não foi encontrado!");
+            return;
+        }
+
+        optionalModelData.get().stream().
+            forEach(m -> System.out.println("Código: " + m.modelCode() +
+                    " Modelo: " + m.modelName()));
+        System.out.println("Digite o código do modelo desejado:");
+        int modelId = scan.nextInt();
+
+        Optional<ModelData> modelFiltered = optionalModelData.get().stream().
+                filter(m -> m.modelCode().equals(modelId))
+                .findFirst();
+
+        if(modelFiltered.isEmpty()) {
+            System.out.println("Não foi encontrado um modelo com esse código!");
+            return;
+        }
+
+        var jsonFipe = fipeApi.getInfoFipe("cars", brandId, modelId, yearId);
+        var fipe = dataConverter.getData(jsonFipe, VehicleFipeData.class);
+
+        System.out.println(
+                "Marca: " + fipe.brand() +
+                "\nModelo: " + fipe.model() +
+                "\nAno: " + fipe.modelYear() +
+                "\nTipo do Combustível: " + fipe.fuel() +
+                "\nPreço fipe: " + fipe.price() +
+                "\nMês de referência: " + fipe.referenceMonth()
+        );
+
+        Vehicle vehicle = new Vehicle(
+                brandId,
+                fipe.brand(),
+                modelId,
+                fipe.model(),
+                yearId,
+                fipe.modelYear(),
+                fipe.fuel(),
+                fipe.price(),
+                fipe.referenceMonth()
+        );
+
+        try {
+            vehicleRepository.save(vehicle);
+            System.out.println("Veículo cadastrado com sucesso!");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 
@@ -148,7 +280,7 @@ public class Main {
         for(BrandData brand : brands) {
             System.out.println("Código da marca: " + brand.brandCode() + " Marca: " + brand.brandName());
         }
-        System.out.println("Digite o código da marca que deseja buscar:");
+        System.out.println("Digite o código da marca do veículo:");
         var brandSearch = scan.nextInt();
         scan.nextLine();
 
