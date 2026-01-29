d3330e1e379899d92dc702b5b7b0cb8c7c00a427
Andreas Santos <andreassantos.3003@gmail.com>
2026-01-28 23:25:41 -0300

    Revert "Retirada de setters do costumer"
    This reverts commit 67559663ed7ec5bd7dc678d082bd0fe083866de5.

--- src/main/java/com/br/appCarros/AplicacaoCarros/model/Costumer.java
+++ src/main/java/com/br/appCarros/AplicacaoCarros/model/Costumer.java
@@ -1,7 +1,5 @@
 package com.br.appCarros.AplicacaoCarros.model;
 
-import com.br.appCarros.AplicacaoCarros.service.Utils.CpfUtils;
-import com.br.appCarros.AplicacaoCarros.service.Utils.PhoneUtils;
 import jakarta.persistence.*;
 
 import java.util.List;
@@ -37,28 +35,40 @@ public class Costumer {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getCpf() {
         return cpf;
     }
 
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
     public String getEmail() {
         return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getPhoneNumber() {
         return phoneNumber;
     }
 
-    public void cleanCpf() {
-        this.cpf = CpfUtils.cleanCpf(this.cpf);
-    }
-
-    public void cleanPhone() {
-        this.phoneNumber = PhoneUtils.cleanPhone(this.phoneNumber);
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 
     @Override
