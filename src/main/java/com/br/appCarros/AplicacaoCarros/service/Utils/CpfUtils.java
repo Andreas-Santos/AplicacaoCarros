df1b933c437eeea64e651dae8bf2805dc54e25d6
Andreas Santos <andreassantos.3003@gmail.com>
2026-01-28 21:34:42 -0300

    Ajustes validador de cpf

--- src/main/java/com/br/appCarros/AplicacaoCarros/service/Utils/CpfUtils.java
+++ src/main/java/com/br/appCarros/AplicacaoCarros/service/Utils/CpfUtils.java
@@ -11,4 +11,36 @@ public class CpfUtils {
 
         return cpf.replaceAll("\\D", "");
     }
+
+    public static boolean isValid(String cpf) {
+        if (cpf == null || cpf.isBlank())
+            return false;
+
+        String cleanedCpf = cleanCpf(cpf);
+
+        if(cleanedCpf.length() != 11)
+            return false;
+
+        if(cleanedCpf.matches("(\\d)\\1{10}"))
+            return false;
+
+        int sum = 0;
+        for(int i = 0; i < 9; i++) {
+            sum += Character.getNumericValue(cleanedCpf.charAt(i) * (10 - i));
+        }
+
+        int digit1 = (sum * 10) % 11;
+        digit1 = digit1 == 10 ? 0 : digit1;
+
+        sum = 0;
+        for(int i = 0; i < 10; i++) {
+            sum += Character.getNumericValue(cleanedCpf.charAt(i) * (11 - i));
+        }
+
+        int digit2 = (sum * 10) % 11;
+        digit2 = digit2 == 10 ? 0 : digit2;
+
+        return (digit1 == Character.getNumericValue(cleanedCpf.charAt(9))) &&
+                (digit2 == Character.getNumericValue(cleanedCpf.charAt(10)));
+    }
 }
