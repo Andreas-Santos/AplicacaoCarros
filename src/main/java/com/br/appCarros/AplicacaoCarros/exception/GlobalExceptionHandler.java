bb071325f485cbbceb26f94f71aeb2bf7fbd9cee
Andreas Santos <andreassantos.3003@gmail.com>
2026-01-28 22:48:45 -0300

    Nova Exception para tratar argumentos faltando ou invalidos

--- src/main/java/com/br/appCarros/AplicacaoCarros/exception/GlobalExceptionHandler.java
+++ src/main/java/com/br/appCarros/AplicacaoCarros/exception/GlobalExceptionHandler.java
@@ -2,10 +2,13 @@ package com.br.appCarros.AplicacaoCarros.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -51,6 +54,25 @@ public class GlobalExceptionHandler {
 
         ResponseError response = new ResponseError(
                 ex.getMessage(),
+                HttpStatus.BAD_REQUEST,
+                LocalDateTime.now()
+        );
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ResponseError> handleValidation(MethodArgumentNotValidException ex) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors()
+                .forEach(error ->
+                        errors.put(error.getField(), error.getDefaultMessage())
+                );
+
+        ResponseError response = new ResponseError(
+                errors.values().toString(),
                 HttpStatus.BAD_REQUEST,
                 LocalDateTime.now()
         );
