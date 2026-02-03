cb795a26ea6ccf716a4e008ca1e0d61ab6d37d2f
Andreas Santos <andreassantos.3003@gmail.com>
2026-02-03 20:37:07 -0300

    Finalizando testes automatizados do vendedor

--- src/test/java/com/br/appCarros/AplicacaoCarros/service/SalesmanServiceTest.java
+++ src/test/java/com/br/appCarros/AplicacaoCarros/service/SalesmanServiceTest.java
@@ -1,5 +1,7 @@
 package com.br.appCarros.AplicacaoCarros.service;
 
+import com.br.appCarros.AplicacaoCarros.exception.CostumerException;
+import com.br.appCarros.AplicacaoCarros.exception.SalesmanException;
 import com.br.appCarros.AplicacaoCarros.mapper.SalesmanMapper;
 import com.br.appCarros.AplicacaoCarros.model.Salesman;
 import com.br.appCarros.AplicacaoCarros.repository.SalesmanRepository;
@@ -12,6 +14,8 @@ import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
 
 import static org.mockito.BDDMockito.*;
 
@@ -48,5 +52,72 @@ class SalesmanServiceTest {
         then(salesmanRepository).should().save(salesmanCaptor.capture());
 
         Assertions.assertEquals(salesmanCaptor.getValue().getName(), salesman.getName());
+    }
+
+    @Test
+    void shouldDeleteSalesmanWhenIdExists() {
+        Long id = 1L;
+        Salesman salesman = new Salesman();
+
+        given(salesmanRepository.findById(id))
+                .willReturn(Optional.of(salesman));
+
+        salesmanService.deleteSalesman(id);
+
+        then(salesmanRepository).should().deleteById(id);
+    }
+
+    @Test
+    void shouldThrowExceptionWhenSalesmanDoesNotExistOnDelete() {
+        Long id = 1L;
+
+        given(salesmanRepository.findById(id))
+                .willReturn(Optional.empty());
+
+        SalesmanException exception = Assertions.assertThrows(
+                SalesmanException.class, () -> salesmanService.deleteSalesman(id)
+        );
+
+        Assertions.assertEquals(
+                "Não existe vendedor com esse id!",
+                exception.getMessage()
+        );
+
+        then(salesmanRepository).should(never()).deleteById(any());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenSalesmanDoesNotExistOnUpdate() {
+        Long id = 1L;
+
+        SalesmanRequest salesmanRequest = mock(SalesmanRequest.class);
+
+        given(salesmanRepository.findById(id))
+                .willReturn(Optional.empty());
+
+        SalesmanException exception = Assertions.assertThrows(
+                SalesmanException.class, () -> salesmanService.updateSalesman(id, salesmanRequest)
+        );
+
+        Assertions.assertEquals(
+                "Não existe vendedor com esse id!",
+                exception.getMessage()
+        );
+    }
+
+    @Test
+    void shouldUpdateSalesmanWhenIdExists() {
+        Long id = 1L;
+
+        Salesman salesman = mock(Salesman.class);
+
+        SalesmanRequest salesmanRequest = mock(SalesmanRequest.class);
+
+        given(salesmanRepository.findById(id))
+                .willReturn(Optional.of(salesman));
+
+        salesmanService.updateSalesman(id, salesmanRequest);
+
+        then(salesman).should().updateSalesman(salesmanRequest);
     }
 }
